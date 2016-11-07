f131e4f7b62afd404e61af5efd7630a64d51f11b
Deepti <deepti.sailam@gmail.com>
2016-11-07 01:03:50 -0500

    Took out boolean inCheck and used isInCheck method instead

--- Chess/src/pieces/King.java
+++ Chess/src/pieces/King.java
@@ -143,6 +143,9 @@ public class King extends Piece {
 		return isInCheck;
 	}
 	
+	//for checkmate - the places around the king are also in check, and the piece(s) that put the king in check are not in check
+	//and no piece of same color can come between king and piece in check
+	
 	public String move(Piece[][] newboard, int srcrow, int srccol, int destrow, int destcol){ //is empty space, so move up, down, left or right one space?
 		if(
 			(srcrow-1==destrow && srccol==destcol) || (srcrow+1==destrow && srccol==destcol) ||
@@ -168,16 +171,16 @@ public class King extends Piece {
 			if(
 				(chess.Board.castling==false) && //there has been no other instance of castling in the game - once per side or game?
 				(moved==false) && //king has not moved
-				(newboard[destrow][destcol].ID=="R") && //theres a Rook
+				(newboard[destrow][destcol].ID=="R") && //there's a Rook
 				(newboard[destrow][destcol].BW==BW) && //Rook is same color as King we want to move
 				(newboard[destrow][destcol].moved==false) && //Rook has also not moved 
-				(newboard[srcrow][srccol].inCheck==false) && //king is not in check
-				(newboard[destrow][destcol].inCheck==false) //destination is not in check
+				(isInCheck(newboard,srcrow,srccol)==false) && //king is not in check
+				(isInCheck(newboard,srcrow,srccol)==false) //destination is not in check
 				
 				) {
 				int c=srccol;
 				for(;c<destcol; c++){
-					if((!(newboard[srcrow][c] instanceof BoardNull)) || (newboard[srcrow][c].inCheck==true)){ //there is a piece b/w king and rook or king will pass through a space that is in check
+					if((!(newboard[srcrow][c] instanceof BoardNull)) || (isInCheck(newboard,srcrow,c)==true)){ //there is a piece b/w king and rook or king will pass through a space that is in check
 						return "Invalid Move. Try Again.\n";
 					}
 				}
